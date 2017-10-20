25d7ffba52cf6c413ad6e579774f79760c0595dd
Kiran Devarakonda <kdevarakonda@us.costar.local>
2017-10-20 18:44:08 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RateLimiter.Services;
+using RateLimiter.Interfaces;
+using Ratelimiter;
+
+
+namespace csharp_client.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IRateLimiterClient rateLimiterService;
+
+        public HomeController(IRateLimiterClient rlService)
+        {
+            rateLimiterService = rlService;
+        }
+        public IActionResult Index()
+        {
+                       
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";            
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+        [Route("Home/Action/{ActionName}")]
+        public JsonResult TakeAction(string ActionName)
+        {
+            var request = new Request();
+            request.Key = ActionName;
+            request.Expire = "60";            
+            return Json(rateLimiterService.checkRequest(request));
+        }
+    }
+}
---
+++ RateLimiter/RateLimiterService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Grpc.Core;
+using Ratelimiter;
+using RateLimiter.Interfaces;
+
+namespace RateLimiter.Interfaces
+{
+    public interface IChannelMaker
+    {
+        Channel MakeRPCChannel();
+    }
+    public interface IRateLimiterClient
+    {
+        State checkRequest(Request request);
+    }
+}
+namespace RateLimiter.Services
+{
+    public class ChannelMaker : Interfaces.IChannelMaker
+    {
+        public Channel MakeRPCChannel()
+        {
+            return new Channel("localhost:50051", ChannelCredentials.Insecure);
+        }
+    }
+    public class RateLimiterClient :IRateLimiterClient
+    {
+        public Channel channel { get; }
+        public Ratelimiter.RateLimiter.RateLimiterClient client { get; }
+
+        public RateLimiterClient(IChannelMaker channelMaker)
+        {
+            channel = channelMaker.MakeRPCChannel();
+            client = new Ratelimiter.RateLimiter.RateLimiterClient(channel);
+        }
+
+        public State checkRequest(Request request)
+        {
+            return client.checkRequest(request);
+        }
+    }
+}
